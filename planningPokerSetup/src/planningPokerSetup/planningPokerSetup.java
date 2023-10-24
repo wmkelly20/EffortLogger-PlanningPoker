8e908dfb21d3ed0b620fbdacd34fd2e931783aa0
wmkelly20 <95336809+wmkelly20@users.noreply.github.com>
2023-10-24 14:23:16 -0700

    Working javafx
    stupdi fucking carter made me do this shit
    now I"m climbing the stairs with Dylan

--- planningPokerSetup/src/planningPokerSetup/planningPokerSetup.java
+++ planningPokerSetup/src/planningPokerSetup/planningPokerSetup.java
@@ -1,10 +1,27 @@
 package planningPokerSetup;
 
-public class planningPokerSetup {
+import javafx.application.Application;  
+import javafx.scene.Scene;  
+import javafx.scene.control.Label;  
+import javafx.scene.layout.StackPane;  
+import javafx.stage.Stage;  
 
+public class planningPokerSetup extends Application {  
+	
+	
+	public void start(Stage primaryStage) throws Exception {  
+		Label my_label=new Label("This is an example of Label");  
+        StackPane root = new StackPane();   
+        Scene scene=new Scene(root,300,300);  
+        root.getChildren().add(my_label);  
+        primaryStage.setScene(scene);  
+        primaryStage.setTitle("Label Class Example");  
+        primaryStage.show();  
+		System.out.println("dyland shmiland");
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("h");
+        launch(args);  
 	}
 
 }
