945a2d82eaeb2c067e8cd12a48ce31147eba0992
s34-m0nst3r <azdylank@gmail.com>
2023-10-24 14:24:11 -0700

    I'M STILL WORKING ON IT
    GO AWAY IT'S IN PROGRESS STFU
    :) 😶

--- DefectLoggerProtoPain/src/DefectLogger.java
+++ DefectLoggerProtoPain/src/DefectLogger.java
@@ -1,7 +1,11 @@
 //package inputPrototype;
 
 
+
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -10,6 +14,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleButton;
@@ -18,6 +23,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
  
 public class DefectLogger extends Application {
@@ -27,42 +33,34 @@ public class DefectLogger extends Application {
     
     public void start(Stage primaryStage) {
     	
-        primaryStage.setTitle("DefectLogger Prototype");
-        Button btn = new Button();
-        btn.setText("SHARE");
-        btn.setOnAction(new EventHandler<>() {
-            public void handle(ActionEvent event) {
-                System.out.println("Shared!");
-            }
-        });
+        primaryStage.setTitle("Defect Logs");
+        
+        //Create the label
+        Label defectConsoleLabel = new Label("Defect Console");
+        defectConsoleLabel.setFont(new Font("Arial", 30));
+        defectConsoleLabel.setStyle("-fx-border-color: black;");
+        defectConsoleLabel.setPadding(new Insets(20));
+        Label projectLabel = new Label("1. Select the Project:");
+        
+        
+        // Create a combo box
+        ObservableList<String> projectOptions = FXCollections.observableArrayList("Development Project","Business Project");
+        ComboBox projectCombo = new ComboBox(projectOptions);
+       
 
-        double checkboxWidth = 150;
-        CheckBox cb1 = new CheckBox("Include Name");
-        cb1.setIndeterminate(false);
-        CheckBox cb2 = new CheckBox("Include Team");
-        cb2.setIndeterminate(false);
-        CheckBox cb3 = new CheckBox("Include Title");
-        cb3.setIndeterminate(false);
-        CheckBox cb4 = new CheckBox("Include Metadata");
-        cb4.setIndeterminate(false);
-        cb1.setMinWidth(checkboxWidth);
-        cb2.setMinWidth(checkboxWidth);
-        cb3.setMinWidth(checkboxWidth);
-        cb4.setMinWidth(checkboxWidth);
         
 
         VBox root = new VBox(10);
         root.setPadding(new Insets(10,10,10,10));
         root.setAlignment(Pos.TOP_CENTER);
         
-        //root.getChildren().add(tb1);
-        primaryStage.setScene(new Scene(root, 300, 250));
+
+        primaryStage.setScene(new Scene(root, 600, 700));
         primaryStage.show();
         
-        //Define label and add everything to vbox
-        Label emptySpace = new Label();
-        emptySpace.setMinHeight(80);
-        root.getChildren().addAll(cb1,cb2,cb3,cb4,emptySpace,btn);
+        //Add children to root
+        root.getChildren().addAll(defectConsoleLabel,projectLabel,projectCombo);
+        
 
         
 
