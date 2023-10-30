36090f9fbc16427b1a28178b6d4f196d3994e0e3
wmkelly20 <95336809+wmkelly20@users.noreply.github.com>
2023-10-29 22:13:27 -0700

    Presentation version
    Doesn't contain completely working code. See previous commit for code to fix to get load to work.

--- planningPokerSetup/src/planningPokerSetup/planningPokerSetup.java
+++ planningPokerSetup/src/planningPokerSetup/planningPokerSetup.java
@@ -51,219 +51,19 @@ Flow:
 */
 	//Class for defects
 	public class Defect{
-		private String project;
-		private String name;
-		private boolean status;
-		private String symptoms;
-		private String stepInject;
-		private String stepRemove;
-		private String defectCategory;
-		private String fixDefect ;
-		
-		public Defect(String project, String name, boolean status, String symptoms, String stepInject, String stepRemove, String defectCategory, String fixDefect)
-		{
-			this.project = project;
-			this.name = name;
-			this.status = status;
-			this.symptoms = symptoms;
-			this.stepInject = stepInject;
-			this.stepRemove = stepRemove;
-			this.defectCategory = defectCategory;
-			this.fixDefect = fixDefect;
-		}
-		
 		//List of Setters for defect class
-		public void setName(String name) {
-			this.name=name;}
-		
-		public void setStatus(boolean status) {
-			this.status = status;}
-		
-		public void setSymptoms(String symptoms) {
-			this.symptoms = symptoms;}
-		
-		public void setStepInject(String stepInject) {
-			this.stepInject = stepInject;}
-		
-		public void setStepRemove(String stepRemove) {
-			this.stepRemove = stepRemove;}
-		
-		public void setDefectCategory(String defectCategory) {
-			this.defectCategory = defectCategory;}
-		
-		public void setFixDefect(String fixDefect) {
-			this.fixDefect = fixDefect;}
-		
-		//List of getters for defect class
-		public String getName() {
-			return name;}
-		public boolean getStatus() {
-			return status;}
-		public String getSymptoms() {
-			return symptoms;}
-		public String getStepInject() {
-			return stepInject;}
-		public String getStepRemove() {
-			return stepRemove;}
-		public String getDefectCategory() {
-			return defectCategory;}
-		public String getFixDefect() {
-			return fixDefect;}
 			
 	}
-	
-	//Core element definitions
-    private ObservableList<String> projectOptions = FXCollections.observableArrayList("Business Project","Development Project");
-    private ComboBox<String> projectCombo = new ComboBox<String>(projectOptions);
-    private ObservableList<Defect> projectDefectsBusiness = FXCollections.observableArrayList(); //This should load up defects that are stored in database
-    private ObservableList<String> projectDefectsBusinessNames = FXCollections.observableArrayList();
-    private ObservableList<Defect> projectDefectsDevelopment = FXCollections.observableArrayList();
-    private ObservableList<String> projectDefectsDevelopmentNames = FXCollections.observableArrayList();
-    private ComboBox<String> defectCombo = new ComboBox<String>(projectDefectsBusinessNames);
-    private ObservableList<String> stepInjectOptions = FXCollections.observableArrayList("Planning","Information Gathering","Information Understanding","Verifying","Outlining","Drafting","Finalizing","Team Meeting","Coach Meeting","Stakeholder Meeting");
-    private ComboBox<String> stepInjectCombo = new ComboBox<String>(stepInjectOptions);
-    private ComboBox<String> stepRemoveCombo = new ComboBox<String>(stepInjectOptions);
-    private ObservableList<String> defectCategoryOptions = FXCollections.observableArrayList("Not Specified", "10 Documentation", "20 Syntax", "30 Build, Package","40 Assignment","50 Interface","60 Checking","70 Data","80 Function", "90 System", "100 Environment");
-    private ComboBox<String> defectCategoryCombo = new ComboBox<String>(defectCategoryOptions);
-    private ComboBox<String> fixDefectCombo = new ComboBox<String>(projectDefectsBusinessNames);
+	    
 
-    
-    private Button newDefectButton = new Button("Create New Defect");
-    private Button updateDefectButton = new Button("Update Current Defect");
-    private Button statusButton = new Button("Toggle Status");
-    private Button saveButton = new Button("Save Data As Defect Log");
     private Button loadButton = new Button("Load Data");
-    private Button deleteButton = new Button("Delete Current Defect");
     
-    //Define core data that the defect log stores
-    private String project = "Business Project";
     private String name = "";
-    private boolean status = false;
     private String symptoms = "";
-    private String stepInject = "";
-    private String stepRemove = "";
-    private String defectCategory = "";
-    private String fixDefect = "";
-    		
 	
-    //Other data in use by defect logger
-    private int defectNumBusiness = 0; //This should be loaded to a value when a project is loaded up
-    private int defectNumDevelopment = 0;
+	@Override public void start(Stage primaryStage) 
+	{  
 	
-	
-	@Override public void start(Stage primaryStage) {  
-	
-		
-		//Action event for load data button
-        EventHandler<ActionEvent> loadEvent = new EventHandler<ActionEvent>() 
-        { 
-        public void handle(ActionEvent e) 
-            {	
-            try {            	
-            	File defectFile = new File("defectLog.txt");
-        	    //check for contents
-        	    if(defectFile.length()==0) {
-        	    	  System.out.println("File is empty");
-        	    }
-        	    else 
-        	    {
-	        	      Scanner defectReader = new Scanner(defectFile);
-	        	      String projectType = defectReader.nextLine();
-	        	      projectType = defectReader.nextLine();
-	        	      
-	        	      //Set which project we are reading to
-	        	      String currentProject = "Business";
-	        	      
-	        	      while (!projectType.equals("$$END READ$$")) 
-	        	      {
-	        	    	//Read in and set all of defect parameters
-	        	    	String loadedName = defectReader.nextLine();
-	        	    	String loadedStatus = defectReader.nextLine();
-	        	    	boolean trueStatus;
-	        	    	if (loadedStatus.equals("false"))
-	        	    		trueStatus = false;
-	        	    	else
-	        	    		trueStatus = true;
-	        	    	String loadedSymptoms = defectReader.nextLine();
-	        	    	String loadedStepInject = defectReader.nextLine();
-	        	    	String loadedStepRemove = defectReader.nextLine();
-	        	    	String loadedDefectCategory = defectReader.nextLine();
-	        	    	String loadedDefectFix = defectReader.nextLine();
-	        	    	  
-	        	    	//Determine which project we're reading to
-	        	    	if (currentProject.equals("Business"))
-	        	    	{
-	            	    	//Create new defect object
-	            	    	Defect newDefect = new Defect("Business Project",loadedName,trueStatus,loadedSymptoms,loadedStepInject,loadedStepRemove,loadedDefectCategory,loadedDefectFix);
-	            	    	//Add these values to the system
-	            	    	defectNumBusiness++;
-	                    	projectDefectsBusiness.addAll(newDefect);
-	                    	projectDefectsBusinessNames.addAll(loadedName);
-	                    	//defectAmount.setText("There are " + Integer.toString(defectNumBusiness) + " defects in this project.");
-	        	    	
-	                    	//Update project combo
-	            	    	projectCombo.setValue("Business Project");
-	            	    	defectCombo.setItems(projectDefectsBusinessNames);
-	                    	fixDefectCombo.setItems(projectDefectsBusinessNames);
-	        	    	
-	        	    	}
-	        	    	else
-	        	    	{
-	        	    		//Create new defect object
-	            	    	Defect newDefect = new Defect("Development Project",loadedName,trueStatus,loadedSymptoms,loadedStepInject,loadedStepRemove,loadedDefectCategory,loadedDefectFix);
-	            	    	//Add these values to the system
-	            	    	defectNumDevelopment++;
-	                    	projectDefectsDevelopment.addAll(newDefect);
-	                    	projectDefectsDevelopmentNames.addAll(loadedName);
-	                    	//defectAmount.setText("There are " + Integer.toString(defectNumDevelopment) + " defects in this project.");
-	        	    	
-	    	        	       
-	            	    	//Update project combo
-	            	    	defectCombo.setItems(projectDefectsDevelopmentNames);
-	                    	fixDefectCombo.setItems(projectDefectsDevelopmentNames);
-	            	    	projectCombo.setValue("Development Project");
-	        	    	}
-	                	
-	                	//Set these values into the current boxes and fields
-	                	defectCombo.setValue(loadedName);
-	                	name = loadedName;
-	                	//to do: fix this later defectNameText.setText(loadedName);
-	                	symptoms = loadedSymptoms;
-	                	//symptomsText.setText(loadedSymptoms);
-	                	
-	                	status = trueStatus;
-	                	//if (trueStatus)
-	                		//statusLabel.setText("Current Status: OPEN");
-	                	//else
-	                		//statusLabel.setText("Current Status: CLOSED");
-	                	stepInjectCombo.setValue(loadedStepInject);
-	                	stepInject = loadedStepInject;
-	                	stepRemoveCombo.setValue(loadedStepRemove);
-	                	stepRemove = loadedStepRemove;
-	                	defectCategoryCombo.setValue(loadedDefectCategory);
-	                	defectCategory = loadedDefectCategory;
-	                	fixDefectCombo.setValue(loadedDefectFix);
-	                	fixDefect = loadedDefectFix;
-	        	    	
-	                	projectType = defectReader.nextLine();
-	                	if (projectType.equals("$$DEVELOPMENT PROJECT$$"))
-	                	{
-	                		currentProject = "Development";
-	                		projectType = defectReader.nextLine();
-	                	}
-	                	
-	        	      }
-	        	      defectReader.close();
-	        	    } 
-        	      	} 
-	            	catch (FileNotFoundException err) {
-	        	      err.printStackTrace();
-	        	    }
-            	}
-            
-        };
-        loadButton.setOnAction(loadEvent);
-                
         Label defectName = new Label("Story:");
         TextField defectNameText = new TextField();
         //Listener for the defectNameBox. When the string within the field has changed
@@ -279,7 +79,7 @@ Flow:
         
         Label symptomsLabel = new Label("Description:");
         TextField symptomsText = new TextField();
-        //This is the textfield for the symptoms. It works functionally the same as the name textfield
+        //Input of symptoms, same as the description of a planning poker session
         symptomsText.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
@@ -291,7 +91,7 @@ Flow:
         Label titleLabel = new Label("Planning Poker:");
         titleLabel.setFont(new Font("Arial", 24));
         
-        Label descLabel = new Label("Please enter your user story below.");
+        Label descLabel = new Label("Please enter a user story below, or load one from the defect logs.");
         
         
         VBox userInput = new VBox(5);          
